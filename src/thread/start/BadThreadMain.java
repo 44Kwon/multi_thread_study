2542116208ac45fa865dc67c4bdd5b275baf20c4
44Kwon <kwh3217@gmail.com>
2024-12-31 15:44:55 +0900

    first commit

---
+++ src/thread/start/BadThreadMain.java
@@ -0,0 +1,15 @@
+package thread.start;
+
+public class BadThreadMain {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + ": main() start");
+
+        HelloThread helloThread = new HelloThread();
+        System.out.println(Thread.currentThread().getName() + ": start() 호출 전");
+        helloThread.run();  //별도의 스레드가 run()을 실행하는것이 아니라 main 스레드가 run()을 호출
+        System.out.println(Thread.currentThread().getName() + ": start() 호출 후");
+
+        System.out.println(Thread.currentThread().getName() + ": main() end");
+    }
+}
